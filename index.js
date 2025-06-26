fb9b9867e3cc4719ac9300ea07a4db4a85c2a690
marzia <you@example.com>
2025-06-26 16:11:02 +0200

    add dotenv configuration and .env file for environment variables

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 //console.log("hello world");
+require("dotenv").config();
+//console.log(process.env); // remove this after you've confirmed it is working
 
 console.log("hello boolean ");
 console.log("dovrebbe funzionare");
